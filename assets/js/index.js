45fe9987a5297b3c20465a935290b63279937211
Ayan863 <GFGayanismayilzade2006@gmail.com>
2024-09-06 21:54:54 +0400

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
-window.onload = function() {
+window.onload = function () {
     const animationContainer = document.querySelector(".animationn");
-    const sections = document.querySelectorAll("section, .content"); // İçerik bölümleri
+    const sections = document.querySelectorAll("section, .content");
 
     sections.forEach(section => section.style.display = "none");
-    animationContainer.style.display = "flex"; // Animasyonu göster
+    animationContainer.style.display = "flex";
 
-    setTimeout(function() {
-        animationContainer.style.display = "none"; // Animasyonu gizle
-        sections.forEach(section => section.style.display = "block"); // İçerikleri geri getir
+    setTimeout(function () {
+        animationContainer.style.display = "none"; 
+        sections.forEach(section => section.style.display = "block"); 
     }, 1000);
 };
 // scrool
@@ -19,3 +19,22 @@ window.addEventListener("scroll", () => {
         header.classList.remove("headerAnime")
     }
 })
+// fish
+function getHtml() {
+    let arr = [245, 182, 1267, 47];
+    let classNames = ["num245", "num182", "num1267", "num47"];
+    let maxValue = Math.max(...arr);
+    arr.forEach((value, index) => {
+        let i = 0;
+        let element = document.querySelector(`.${classNames[index]}`);
+        if (element) {
+            let interval = setInterval(function () {
+                element.innerHTML = `${i}`;
+                i++;
+                if (i > value) clearInterval(interval);
+            }, maxValue / value * 6);
+        }
+    });
+}
+getHtml()
+
